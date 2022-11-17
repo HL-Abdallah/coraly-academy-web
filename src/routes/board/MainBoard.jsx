a0ed1e267470809a3bc4cc85575932a0900a90c1
Abdallah <hameurlain.abdallah@gmail.com>
2022-11-17 18:13:02 +0100

    finished main board page

--- src/routes/board/MainBoard.jsx
+++ src/routes/board/MainBoard.jsx
@@ -1,8 +1,12 @@
-import { Typography } from "@mui/material";
+import { Button, Dialog, TextField, Typography } from "@mui/material";
 import ProcessCard from "../../components/reusable/ProcessCard";
 import { processList } from "../../data/process";
+import newProcess from "/icons/process/new-process.png";
+import { useState } from "react";
 
 const MainBoard = () => {
+  const [dialogOpen, setDialogOpen] = useState(false);
+
   return (
     <div
       style={{
@@ -33,16 +37,47 @@ const MainBoard = () => {
         Work with your team mates, take track of your process and <br /> close
         all tasks
       </Typography>
-      {/* div with centered blocks */}
       <div
+        id="processes-container"
         style={{
           display: "flex",
           flexWrap: "wrap",
           gap: "24px",
         }}
       >
+        <div
+          onClick={() => {
+            // alert("new process created");
+            setDialogOpen(true);
+          }}
+        >
+          <div
+            id="new-process-btn"
+            style={{
+              height: "150px",
+              width: "150px",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              border: "1px dashed #6F6D7B",
+            }}
+          >
+            <img src={newProcess} alt="New Process" />
+            <p
+              style={{
+                color: "#6F6D7B",
+                fontWeight: "600",
+                fontSize: "16px",
+                textAlign: "center",
+              }}
+            >
+              Create a new
+              <br /> process
+            </p>
+          </div>
+        </div>
         {processList.map((item) => {
-          console.log(item);
           return (
             <ProcessCard
               key={item.processName}
@@ -54,6 +89,71 @@ const MainBoard = () => {
           );
         })}
       </div>
+      <Dialog open={dialogOpen} onClose={() => setDialogOpen(false)}>
+        <div
+          style={{
+            width: "600px",
+            height: "292px",
+            padding: "24px",
+          }}
+        >
+          <div
+            id="new-process-top-section"
+            style={{
+              paddingBottom: "24px",
+            }}
+          >
+            <p
+              style={{
+                fontWeight: "700",
+                fontSize: "20px",
+                color: "#464356",
+                margin: "0px",
+                padding: "0px",
+              }}
+            >
+              Create a new process
+            </p>
+          </div>
+          <div
+            id="new-process-mid-section"
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "24px",
+            }}
+          >
+            <TextField label="Process name" fullWidth />
+            <TextField label="Color" fullWidth />
+          </div>
+          <div
+            id="new-process-bot-section"
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              paddingTop: "24px",
+              gap: "24px",
+            }}
+          >
+            <Button color="error" variant="outlined">
+              Annulla
+            </Button>
+            <Button
+              variant="contained"
+              sx={{
+                color: "#white",
+                backgroundColor: "#2CCFBC",
+                "&:hover": {
+                  color: "#white",
+                  backgroundColor: "#2CCFBC",
+                },
+              }}
+            >
+              Crea
+            </Button>
+          </div>
+        </div>
+      </Dialog>
     </div>
   );
 };
